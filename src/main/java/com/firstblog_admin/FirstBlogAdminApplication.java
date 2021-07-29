63041a3ff24b08109d2c5e01e0ed5793a8496d06
knight1527 <1518607977@qq.com>
2021-07-29 22:50:14 +0800

    first blog admin springboot 7.29

---
+++ src/main/java/com/firstblog_admin/FirstBlogAdminApplication.java
@@ -0,0 +1,16 @@
+package com.firstblog_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author knight1527
+ */
+@SpringBootApplication
+public class FirstBlogAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstBlogAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/firstblog_admin/FirstBlogAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstblog_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBlogAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
